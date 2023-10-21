e98c516444672971b7fc1f9297a3737226337367
Jaw <58354193+wajeht@users.noreply.github.com>
2023-10-21 15:11:21 -0500

    feat: pm2 script

---
+++ cron.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# set -e
+
+if ! command -v pm2 &> /dev/null; then
+  echo "Installing PM2..."
+  npm install pm2 -g
+fi
+
+if pm2 show cron &> /dev/null; then
+  echo "Stopping and deleting existing cron..."
+  pm2 stop cron
+  pm2 delete cron
+fi
+
+# 30 days * 24 hours * 60 minutes * 60 seconds * 1000 milliseconds
+KILL_TIMEOUT_MS=$((30 * 24 * 60 * 60 * 1000))
+
+pm2 start ./index.js --name cron --kill-timeout $KILL_TIMEOUT_MS
+pm2 startup
+pm2 save --force
+
+echo ""
+echo "cron scheduler has been started"
+echo ""
+# exit 0
